83759303bc79e458a270f13a8e88f4e4e8c8a8e2
Muhin <mohinstudent2000@gmail.com>
2026-03-08 01:41:26 +0600

    added application db context

---
+++ src/Raksha.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Raksha.Infrastructure.Identity;
+
+namespace Raksha.Infrastructure.Data
+{
+    internal class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+    }
+}
