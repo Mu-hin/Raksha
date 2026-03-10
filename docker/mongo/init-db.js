1658073afd7d73f738b3499fe1b29aad23c37613
Muhin <muhin.onnorokom@gmail.com>
2026-03-10 20:23:17 +0600

    added docker compose

---
+++ docker/mongo/init-db.js
@@ -0,0 +1,2 @@
+db = db.getSiblingDB('auth-demo');
+db.createCollection('audit_logs');
---
+++ docker/postgres/init-db.sql
@@ -0,0 +1,3 @@
+-- Database 'auth-demo' is auto-created by POSTGRES_DB env var
+-- Add any extensions needed
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
